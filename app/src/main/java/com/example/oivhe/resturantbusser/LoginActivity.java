eaf0e7783ff17b0687e7bd3d87d0d0c54bb19887
oivheg <oivheg@gmail.com>
2017-02-28 08:37:58 +0100

    - login activity starts Activeuser Activity when it is finished . - active User activiyt now starts, and have 2 buttons

--- app/src/main/java/com/example/oivhe/resturantbusser/LoginActivity.java
+++ app/src/main/java/com/example/oivhe/resturantbusser/LoginActivity.java
@@ -372,8 +372,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-
+                Intent intent = new Intent(LoginActivity.this,ActiveUser.class);
+                LoginActivity.this.startActivity(intent);
                 finish();
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
