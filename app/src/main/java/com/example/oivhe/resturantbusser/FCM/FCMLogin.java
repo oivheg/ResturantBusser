6d5da74027fff132aefed7f544e63f8925146f2b
oivheg <oivheg@gmail.com>
2017-03-23 15:46:01 +0100

    Some changes to the code, as well as firebas instanse added

--- app/src/main/java/com/example/oivhe/resturantbusser/FCM/FCMLogin.java
+++ app/src/main/java/com/example/oivhe/resturantbusser/FCM/FCMLogin.java
@@ -1,4 +1,4 @@
-package com.example.oivhe.resturantbusser;
+package com.example.oivhe.resturantbusser.FCM;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,12 +12,13 @@ import android.widget.Toast;
 
 import com.example.oivhe.resturantbusser.Communication.BusserRestClient;
 import com.example.oivhe.resturantbusser.GUI.ActiveUser;
+import com.example.oivhe.resturantbusser.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GetTokenResult;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -37,6 +38,7 @@ public class FCMLogin extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_fcmlogin);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        FirebaseApp.initializeApp(this);
         mAuth = FirebaseAuth.getInstance();
 
         mAuthListener = new FirebaseAuth.AuthStateListener() {
--- app/src/main/java/com/example/oivhe/resturantbusser/MainActivity.java
+++ app/src/main/java/com/example/oivhe/resturantbusser/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.oivhe.resturantbusser;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.oivhe.resturantbusser.GUI.ActiveUser;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.oivhe.resturantbusser.FCM.FCMLogin;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
